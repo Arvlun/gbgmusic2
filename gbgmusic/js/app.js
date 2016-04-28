99dc7937f77aa107b1aa92748bf3c4567acf6b4b
arvidw <arvidwessman@gmail.com>
2016-04-28 13:05:49 +0200

    skit

--- gbgmusic/js/app.js
+++ gbgmusic/js/app.js
@@ -7,11 +7,8 @@
 		view: 'month',
 		tmpl_path: 'tmpls/',
 		tmpl_cache: false,
-<<<<<<< HEAD
 		day: '2016-06-01',
-=======
-		day: '2016-05-20',
->>>>>>> origin/master
+
 		onAfterEventsLoad: function(events) {
 			if(!events) {
 				return;
