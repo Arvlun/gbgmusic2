41d5989b8153ca320515c6ebcf2a622ea42a084b
Bananjocke <arvid.lundin8907@gmail.com>
2016-04-30 16:03:23 +0200

    lägg till och visar informationen i modal när du trycker
    tryck på add bredvid <<prev för att få upp formen för att lägga till

--- gbgmusic/events.json.php
+++ gbgmusic/events.json.php
@@ -1,77 +1,38 @@
 {
-	"success": 1,
-	"result": [
-		{
-			"id": "293",
-			"title": "Two Year Vacation @ Bar Robusta",
-			"url": "https://www.facebook.com/events/1199737493372484/",
-			"class": "event-warning",
-			"start": "1460791600000",
-			"end":   "1460791900000"
-		},
-		{
-			"id": "256",
-			"title": "Event that ends on timeline",
-			"url": "http://www.example.com/",
-			"class": "event-warning",
-			"start": "1363155300000",
-			"end":   "1363227600000"
-		},
-		{
-			"id": "276",
-			"title": "Short day event",
-			"url": "http://www.example.com/",
-			"class": "event-success",
-			"start": "1363245600000",
-			"end":   "1363252200000"
-		},
-		{
-			"id": "294",
-			"title": "This is information class ",
-			"url": "http://www.example.com/",
-			"class": "event-info",
-			"start": "1363111200000",
-			"end":   "1363284086400"
-		},
-		{
-			"id": "297",
-			"title": "This is success event",
-			"url": "http://www.example.com/",
-			"class": "event-success",
-			"start": "1363234500000",
-			"end":   "1363284062400"
-		},
-		{
-			"id": "54",
-			"title": "This is simple event",
-			"url": "http://www.example.com/",
-			"class": "",
-			"start": "1363712400000",
-			"end":   "1363716086400"
-		},
-		{
-			"id": "532",
-			"title": "This is inverse event",
-			"url": "http://www.example.com/",
-			"class": "event-inverse",
-			"start": "1364407200000",
-			"end":   "1364493686400"
-		},
-		{
-			"id": "548",
-			"title": "This is special event",
-			"url": "http://www.example.com/",
-			"class": "event-special",
-			"start": "1363197600000",
-			"end":   "1363629686400"
-		},
-		{
-			"id": "295",
-			"title": "Event 3",
-			"url": "http://www.example.com/",
-			"class": "event-important",
-			"start": "1364320800000",
-			"end":   "1364407286400"
-		}
-	]
+    "success": 1,
+    "result": [
+        {
+            "id": "293",
+            "title": "Erik Pontiak",
+            "url": "erik.html",
+            "class": "event-warning",
+            "location": "Hemma hos Erik",
+            "bandname": "Erik Pontiak",
+            "price": 100,
+            "start": "1460791600000",
+            "end": "1460791900000"
+        },
+        {
+            "id": "1336",
+            "title": "whatevs",
+            "url": "www.dsadsa.com",
+            "class": "event-success",
+            "location": "Bonden",
+            "bandname": "Band of brothers",
+            "price": "23",
+            "start": "1460791600000",
+            "end": "1460791900000"
+        },
+        {
+            "id": "233",
+            "title": "TITLETITELKTE",
+            "url": "43.com",
+            "class": "event-warning",
+            "location": "Duvet",
+            "bandname": "inte",
+            "price": "23",
+            "start": "1462022580000",
+            "end": "1462029840000"
+        }
+    ]
 }
---
+++ gbgmusic/saveform.php
@@ -0,0 +1,22 @@
+<?php
+
+//$start=$_POST['start'];
+//echo strtotime($start), "000\n";
+
+$json = file_get_contents('events.json.php');
+$data = json_decode($json, true);
+$starttime = strtotime($_POST['start'])."000";
+$endtime = strtotime($_POST['end'])."000";
+array_push($data['result'], array(
+      'id'=> $_POST['id'],
+      'title'=> $_POST['title'],
+      'url'=> $_POST['url'],
+      'class'=> $_POST['class'],
+      'location'=> $_POST['location'],
+      'bandname'=> $_POST['bandname'],
+      'price'=> $_POST['price'],
+      'start'=> $starttime,
+      'end'=> $endtime));
+file_put_contents('events.json.php', json_encode($data, JSON_PRETTY_PRINT));	
+	
+?>
