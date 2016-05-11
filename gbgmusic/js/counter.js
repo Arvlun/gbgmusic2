06b0aeaf22055c7cc52bf9bb427ea6f6c7a6443c
arvidw <arvidwessman@gmail.com>
2016-05-11 15:21:00 +0200

    Descriptionbox med counter

---
+++ gbgmusic/js/counter.js
@@ -0,0 +1,22 @@
+var main = function() {
+    $('.btn').click(function(){
+     var post = $('.status-box').val();
+     $('<li>').text(post).prependTo('.posts');
+     $('.status-box').val('');
+     $('.counter').text('140');
+    });
+
+        $('.status-box').keyup(function(){
+            var postLength = $(this).val().length;
+            var charactersLeft = 140-postLength;
+            
+            $('.counter').text(charactersLeft);
+
+            if(charactersLeft < 0){
+                $('.btn-primary').addClass('disabled');
+            }
+            else {  $('.btn-primary').removeClass('disabled');}
+        });
+};
+
+$(document).ready(main);
